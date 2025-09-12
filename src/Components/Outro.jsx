9fbc376a5e8a67b517bc0cb6ec863cea4e162dbf
Gaurav Shambharkar <shambharkargaurav04@gmail.com>
2025-09-12 21:12:54 +0530

    shinyTxt

--- src/Components/Outro.jsx
+++ src/Components/Outro.jsx
@@ -1,13 +1,21 @@
 import React from "react";
+import ShinyText from "./AnimationComponents/ShinyText";
 
 const Outro = () => {
   return (
     <>
       <div className="w-full xcenter ">
         <div className="w-190 text-white borderb p-4 xcenter gap-5 ">
-          <h1 className="text-9xl bg-gradient-to-b  from-[#ffffff]  to-[#1b1b1b] inset-shadow-2xs font-bold max-[700px]:text-7xl bg-clip-text text-transparent">
+          <h1 className="text-9xl relative bg-gradient-to-b  from-[#ffffff]  to-[#0e0e0e] inset-shadow-2xs font-bold max-[700px]:text-7xl bg-clip-text text-transparent">
             GAURAV.
+            
           </h1>
+          <ShinyText
+            text="GAURAV."
+            disabled={false}
+            speed={5}
+            className="custom-class text-9xl absolute bg-gradient-to-b  from-[#ffffff]  to-[#1b1b1b] inset-shadow-2xs font-bold max-[700px]:text-7xl bg-clip-text text-transparent"
+         />
         </div>
       </div>
     </>
