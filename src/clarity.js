bbd7bf408c09e3a6ca3eebc51f5421d198048da7
Gaurav Shambharkar <shambharkargaurav04@gmail.com>
2025-10-14 14:10:36 +0530

    init

--- src/clarity.js
+++ src/clarity.js
@@ -1,20 +1,19 @@
-// clarity.js
 function loadClarity(tagId) {
-  return new Promise((resolve) => {
-    if (window.clarity) return resolve();
+  // Check if Clarity is already loaded
+  if (window.clarity) return;
 
-    window.clarity = window.clarity || function () {
+  window.clarity =
+    window.clarity ||
+    function () {
       (window.clarity.q = window.clarity.q || []).push(arguments);
     };
 
-    const script = document.createElement("script");
-    script.async = true;
-    script.src = "https://www.clarity.ms/tag/" + tagId;
-    script.onload = () => resolve();
+  const script = document.createElement("script");
+  script.async = true;
+  script.src = "https://www.clarity.ms/tag/" + tagId;
 
-    const firstScript = document.getElementsByTagName("script")[0];
-    firstScript.parentNode.insertBefore(script, firstScript);
-  });
+  const firstScript = document.getElementsByTagName("script")[0];
+  firstScript.parentNode.insertBefore(script, firstScript);
 }
 
 export default loadClarity;
